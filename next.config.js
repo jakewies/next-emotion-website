29d327a7982e4eb3bbefef7d41c635c946a366fe
Jake Wiesler <jakewiesler@gmail.com>
2017-08-16 20:06:00 -0400

    Added a custom next.config.js file for eslint-loader support in webpack.
    This loader watches for file changes and, as long as the dev server is running, will print errors and warnings to the console without
    having to run npm run lint.

---
+++ next.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  // Performs customizations to webpack config
+  webpack: (config, {dev}) => {
+    if (dev) {
+      // Adds ESLint support
+      config.module.rules.push({
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader'
+      })
+    }
+    return config
+  }
+}
