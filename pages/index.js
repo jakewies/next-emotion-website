0a4e404f6b21a3b231bd42667f87e6cab27d2dbf
Jake Wiesler <jakewiesler@gmail.com>
2017-08-17 00:34:36 -0400

    Add support for emotion, a css-in-js library. Create a custom .babelrc configuration file. Add babel-preset-stage-0.

--- pages/index.js
+++ pages/index.js
@@ -2,24 +2,48 @@ import React from 'react'
 import Layout from '../components/Layout'
 import Link from 'next/link'
 import PropTypes from 'prop-types'
+import { css } from 'emotion'
+import styled from 'emotion/react'
+
+const fontFamily = css`
+  font-family: 'Arial';
+`
+
+const Anchor = styled.a`
+  composes: ${fontFamily};
+  text-decoration: none;
+  color: blue;
+  cursor: pointer;
+
+  &:hover {
+    opacity: 0.6;
+  }
+`
+
+const List = styled.ul`padding: 0;`
+
+const Item = styled.li`
+  list-style: none;
+  margin: 5px 0;
+`
 
 const PostLink = props =>
-  <li>
+  <Item>
     <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
-      <a>
+      <Anchor>
         {props.title}
-      </a>
+      </Anchor>
     </Link>
-  </li>
+  </Item>
 
 const Index = () =>
   <Layout>
-    <h1>My Blog</h1>
-    <ul>
+    <h1 className={fontFamily}>My Blog</h1>
+    <List>
       <PostLink id="hello-nextjs" title="Hello Next.js" />
       <PostLink id="learn-nextjs" title="Learn Next.js is awesome" />
       <PostLink id="deploy-nextjs" title="Deploy apps with Zeittt" />
-    </ul>
+    </List>
   </Layout>
 
 PostLink.propTypes = {
