66b0a803f6302731d142db09e89f09c03c070530
Jake Wiesler <jakewiesler@gmail.com>
2017-08-18 17:22:35 -0400

    Add static HTML deployment support.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 module.exports = {
+  exportPathMap: function() {
+    return {
+      '/': { page: '/' },
+      '/p/hello-nextjs': { page: '/post', query: { title: 'Hello Next.js' } },
+      '/p/learn-nextjs': {
+        page: '/post',
+        query: { title: 'Learn Next.js is awesome' }
+      },
+      '/p/deploy-nextjs': {
+        page: '/post',
+        query: { title: 'Deploy apps with Zeit' }
+      }
+    }
+  },
   // Performs customizations to webpack config
   webpack: (config, { dev }) => {
     if (dev) {
