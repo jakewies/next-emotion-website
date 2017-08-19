2245bad318061db98cc4a540e29640ec43580e83
Jake Wiesler <jakewiesler@gmail.com>
2017-08-19 12:16:08 -0400

    Add client and server fetching of blog posts from a json file at the root of the project.
    This commit contains a working prototype of a blog with dynamic routes on the client and server. Unfortunately the goal
    of my website is to be 100% static. This prototype will not suffice. It relies on the req parameter inside getInitialProps, which
    is not available when next runs next export.

--- pages/blog.js
+++ pages/blog.js
@@ -2,37 +2,41 @@ import React from 'react'
 import Layout from '../components/Layout'
 import Link from 'next/link'
 import PropTypes from 'prop-types'
-
-function getPosts() {
-  return [
-    { id: 'hello-nextjs', title: 'Hello Next.js' },
-    { id: 'learn-nextjs', title: 'Learn Next.js is awesome' },
-    { id: 'deploy-nextjs', title: 'Deploy apps with ZEIT' }
-  ]
-}
+import axios from 'axios'
 
 const PostLink = props =>
   <li>
-    <Link as={`/blog/${props.id}`} href={`/post?title=${props.title}`}>
+    <Link as={`/blog/${props.id}`} href={`/post?id=${props.id}`}>
       <a>
         {props.title}
       </a>
     </Link>
   </li>
 
-const Index = () =>
+const Index = props =>
   <Layout>
     <h1>Blog</h1>
     <ul>
-      {getPosts().map(post =>
-        <PostLink key={post.id} id={post.id} title={post.title} />
+      {Object.keys(props.posts).map(id =>
+        <PostLink key={id} id={id} title={props.posts[id].title} />
       )}
     </ul>
   </Layout>
 
+Index.getInitialProps = async function({ req }) {
+  // If server, req will be defined
+  const baseUrl = req ? `${req.protocol}://${req.get('Host')}` : ''
+  const res = await axios.get(`${baseUrl}/api/posts`)
+  return { posts: res.data }
+}
+
 PostLink.propTypes = {
   id: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired
+}
+
+Index.propTypes = {
+  posts: PropTypes.object.isRequired
 }
 
 export default Index
--- pages/post.js
+++ pages/post.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import axios from 'axios'
 
 /* eslint react/prop-types: 0 */
 
 const Post = props =>
   <Layout>
     <h1>
-      {props.url.query.title}
+      {props.post.title}
     </h1>
-    <p>This is the blog post content.</p>
+    <p>
+      {props.post.content}
+    </p>
   </Layout>
+
+Post.getInitialProps = async function({ req, query }) {
+  // If server, req will be defined
+  const baseUrl = req ? `${req.protocol}://${req.get('Host')}` : ''
+  const res = await axios.get(`${baseUrl}/api/posts/${query.id}`)
+  return { post: res.data }
+}
 
 export default Post
